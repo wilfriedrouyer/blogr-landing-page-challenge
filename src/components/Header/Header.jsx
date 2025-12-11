c6b583606cb91b18700564db386e3a77dda3bdd1
Wilfried ROUYER <wilfried.rouyer57@hotmail.fr>
2025-12-11 17:03:24 +0100

    Feat: Ajout de la navbar au Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from './Navbar'
+
+import "../../styles/Header.css"
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <Navbar />
+        
+    </div>
+  )
+}
