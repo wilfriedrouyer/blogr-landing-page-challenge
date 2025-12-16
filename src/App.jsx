14e86535b783a99a59bd1b85e9bf83967bf4405a
Wilfried ROUYER <wilfried.rouyer57@hotmail.fr>
2025-12-16 17:18:38 +0100

    Feat: Créations de la page Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./pages/Home";
 import Header from "./components/Header/HeroSection";
 
 import "./styles/App.css"
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Infrastructure from "../components/Home/Infrastructure";
+import Introduction from "../components/Home/Introduction";
+import Tooling from "../components/Tooling";
+
+export default function Home() {
+  return (
+    <>
+      <Introduction />
+      <Infrastructure />
+      <Tooling />      
+    </>
+  )
+}
