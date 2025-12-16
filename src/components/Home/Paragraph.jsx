25e81e078e05acf54e580769db4f12df7b383ce2
Wilfried ROUYER <wilfried.rouyer57@hotmail.fr>
2025-12-16 17:18:13 +0100

    Feat: Création des sections et des paragraphes

---
+++ src/components/Home/Paragraph.jsx
@@ -0,0 +1,13 @@
+import '../../styles/Home/Paragraph.css'
+
+export default function Paragraph({title, style, children}) {
+
+
+
+  return (
+    <div className='paragraph' style={style}>
+        {title && <h2 className='paragraph-title' style={style?.contentStyle}>{title}</h2>}
+        <div className='paragraph-content' style={style?.contentStyle}>{children}</div>
+    </div>
+  )
+}
