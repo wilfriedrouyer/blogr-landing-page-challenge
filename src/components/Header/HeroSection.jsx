6fc6a39c5393aed4485c7610fe380b9ab4521e3f
Wilfried ROUYER <wilfried.rouyer57@hotmail.fr>
2025-12-16 11:33:29 +0100

    Feat: Ajout du contenu du Hero Section

--- src/components/Header/HeroSection.jsx
+++ src/components/Header/HeroSection.jsx
@@ -1,12 +1,19 @@
 import Navbar from './Navbar'
 
-import "../../styles/Header.css"
+import "../../styles/HeroSection.css"
 
-export default function Header() {
+export default function HeroSection() {
   return (
-    <div className='header'>
+    <div className='hero-section'>
         <Navbar />
-        
+        <div className='hero-content'>
+          <div className='headline'>A modern publishing platform</div>
+          <div className='subheadline'>Grow your audience and build your online brand</div>
+          <div className='hero-cta'>
+            <a href='/' className='cta start-for-free'>Start for Free</a>
+            <a href='/' className='cta learn-more'>Learn more</a>
+          </div>
+        </div>
     </div>
   )
 }
