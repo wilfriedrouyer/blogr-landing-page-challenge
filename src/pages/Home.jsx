64d4f49f14e46bafeca1dd1aea6624e9ce0ce182
Wilfried ROUYER <wilfried.rouyer57@hotmail.fr>
2025-12-17 12:59:27 +0100

    Feat: Création du style de la page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import Infrastructure from "../components/Home/Infrastructure";
 import Introduction from "../components/Home/Introduction";
 import Tooling from "../components/Tooling";
 
+import '../styles/Home.css'
+
 export default function Home() {
   return (
-    <>
+    <div className="home">
       <Introduction />
       <Infrastructure />
       <Tooling />      
-    </>
+    </div>
   )
 }
