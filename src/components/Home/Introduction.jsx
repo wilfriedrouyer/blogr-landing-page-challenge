5d198f96a18c0b69f8d5b336533e6265fc17fb66
Wilfried ROUYER <wilfried.rouyer57@hotmail.fr>
2025-12-17 12:59:12 +0100

    Feat: Ajout du titre à la page

--- src/components/Home/Introduction.jsx
+++ src/components/Home/Introduction.jsx
@@ -1,6 +1,8 @@
 import Paragraph from '../Paragraph'
 import Section from '../Section'
 
+import '../../styles/Home/Introduction.css'
+
 import editorImg from '../../assets/img/illustration-editor-desktop.svg'
 
 export default function Introduction() {
@@ -8,17 +10,20 @@ export default function Introduction() {
     const stickSection = 'right';
 
     return (
-        <Section image={editorImg} imagePosition={imagePosition} stickSection={stickSection}>
-            <Paragraph title={"Introducing an extensible editor"}>
-            Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
-            The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
-            videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
-            change the looks of a blog.
-            </Paragraph>
-            <Paragraph title={"Robust content management"}>
-            Flexible content management enables users to easily move through posts. Increase the usability of your blog 
-            by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
-            </Paragraph>
-        </Section>
+        <>
+            <div className='home-title'>Designed for the future</div>
+            <Section image={editorImg} imagePosition={imagePosition} stickSection={stickSection}>
+                <Paragraph title={"Introducing an extensible editor"}>
+                Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
+                The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
+                videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
+                change the looks of a blog.
+                </Paragraph>
+                <Paragraph title={"Robust content management"}>
+                Flexible content management enables users to easily move through posts. Increase the usability of your blog 
+                by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
+                </Paragraph>
+            </Section>
+        </>
     )
 }
